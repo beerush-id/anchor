3e5c0ef98cdec890e85adf3c1d126acd06c1e09f
Nanang Mahdaen El Agung <mahdaen@gmail.com>
2025-08-16 00:44:34 +0700

    style(docs): format code with Prettier and update ESLint config
    - Update Prettier configuration to use LF line endings
    - Remove trailing commas and adjust print width to 100 characters
    - Update ESLint configuration to use consistent formatting
    - Remove redundant comments and adjust spacing

--- apps/docs/eslint.config.js
+++ apps/docs/eslint.config.js
@@ -20,16 +20,14 @@ export default ts.config(
 		languageOptions: {
 			globals: { ...globals.browser, ...globals.node }
 		},
-		rules: { // typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
-		// see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
-		"no-undef": 'off' }
+		rules: {
+			// typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
+			// see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
+			'no-undef': 'off'
+		}
 	},
 	{
-		files: [
-			'**/*.svelte',
-			'**/*.svelte.ts',
-			'**/*.svelte.js'
-		],
+		files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
 		languageOptions: {
 			parserOptions: {
 				projectService: true,
