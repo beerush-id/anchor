bb294a49fe1fe1c7c677f6612049010f935ac85c
Nanang Mahdaen El Agung <mahdaen@gmail.com>
2025-08-19 05:25:25 +0700

    test: configure root Vitest for unit testing with coverage reporting
    - Update package.json to use Vitest for testing
    - Add test:preview script for previewing coverage report
    - Configure Vitest to include specific test files and generate HTML coverage report

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,13 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    root: './test',
+    include: ['packages/core/test/**/*.{test,spec}.{ts,js}', 'packages/storage/test/**/*.{test,spec}.{ts,js}'],
+    reporters: ['default', 'html'],
+    outputFile: 'coverage/index.html',
+    coverage: {
+      enabled: true,
+      include: ['packages/core/src/**/*.ts', 'packages/storage/src/**/*.ts'],
+      reportsDirectory: './coverage/coverage',
+    },
   },
 });
