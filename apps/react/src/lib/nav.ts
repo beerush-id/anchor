dcad77c4ca07591934c99433f4cc424618236f01
Nanang Mahdaen El Agung <mahdaen@gmail.com>
2025-09-18 18:40:25 +0700

    build: add deployment scripts and adjust base paths for different environments
    - Add build-cf.sh and build-vercel.sh scripts for Cloudflare and Vercel deployments
    - Modify base paths in nav.ts and docs config to support worker builds
    - Update environment variable usage for more flexibility in deployment configurations

--- apps/react/src/lib/nav.ts
+++ apps/react/src/lib/nav.ts
@@ -2,7 +2,7 @@ import type { MouseEventHandler } from 'react';
 import { getContext, setContext } from '@anchorlib/core';
 import { useVariable, type VariableRef } from '@anchorlib/react';
 
-export const BASE_PATH = '/anchor';
+export const BASE_PATH = !process.env.WORKER_BUILD ? '/anchor' : '';
 
 export const inlineNav: MouseEventHandler = (event) => {
   const href = (event.currentTarget as HTMLAnchorElement)?.getAttribute('href');
