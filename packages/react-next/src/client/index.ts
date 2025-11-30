97cda41378242dee1f09a19314324d7a58191b59
Nanang Mahdaen El Agung <mahdaen@gmail.com>
2025-11-30 18:45:16 +0700

    refactor(react-next): remove deprecated react-next package
    - Removed entire react-next package implementation in favor of migration to react package

--- packages/react-next/src/client/index.ts
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { useEffect, useMemo, useRef, useState } from 'react';
-import { setEffectHook, setMemoHook, setRefHook, setStateHook } from '../hooks.js';
-
-setRefHook(useRef);
-setMemoHook(useMemo);
-setStateHook(useState);
-setEffectHook(useEffect);
--- packages/react-next/src/core/index.ts
+++
@@ -1,1 +0,0 @@
-export * from '@anchorlib/core';
