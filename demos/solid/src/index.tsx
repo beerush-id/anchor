00a04a94ee622b979dcf8bb92ee55ca2d721e436
Nanang Mahdaen El Agung <mahdaen@gmail.com>
2026-01-09 12:34:08 +0700

    refactor(demo): move demo apps to the demos workspace

--- demos/solid/src/index.tsx
+++ demos/solid/src/index.tsx
@@ -1,8 +1,8 @@
 /* @refresh reload */
-import { render } from 'solid-js/web'
-import './index.css'
-import App from './App.tsx'
+import { render } from 'solid-js/web';
+import './index.css';
+import App from './App.tsx';
 
-const root = document.getElementById('root')
+const root = document.getElementById('root');
 
-render(() => <App />, root!)
+render(() => <App />, root!);
