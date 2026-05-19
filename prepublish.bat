87e73b8330825dfbaacf19bc13ab8bcf3a4d4591
Nanang Mahdaen El Agung <mahdaen@gmail.com>
2026-05-19 16:19:17 +0700

    chore(release): remove react-classic from prepublish scripts
    Streamline the release process by removing the `react-classic` package from the prepublish scripts (`prepublish.bat` and `prepublish.sh`). This change excludes `react-classic` from the automated versioning logic that updates `@anchorlib/core` and `@anchorlib/storage` dependencies, effectively decoupling it from the release cycle.
    
    - Remove `packages/react-classic` from dependency update commands in `prepublish.bat`.
    - Remove `packages/react-classic` from dependency update commands in `prepublish.sh`.

--- prepublish.bat
+++ prepublish.bat
@@ -2,7 +2,7 @@ bun run prepublish
 
 set /p VERSION="Enter version: "
 
-bpkg info set dependencies.@anchorlib/core="^%VERSION%" -f packages/storage packages/react packages/router packages/react-classic packages/solid packages/svelte packages/vue irpclib/irpc
+bpkg info set dependencies.@anchorlib/core="^%VERSION%" -f packages/storage packages/react packages/router packages/solid packages/svelte packages/vue irpclib/irpc
 bpkg info set dependencies.@anchorlib/router="^%VERSION%" -f packages/react
-bpkg info set dependencies.@anchorlib/storage="^%VERSION%" -f packages/react packages/react-classic packages/solid packages/svelte packages/vue
+bpkg info set dependencies.@anchorlib/storage="^%VERSION%" -f packages/react packages/solid packages/svelte packages/vue
 bpkg info set peerDependencies.@irpclib/irpc="^%VERSION%" -f irpclib/http irpclib/ws irpclib/broadcast
--- prepublish.sh
+++ prepublish.sh
@@ -3,7 +3,7 @@ bun run prepublish
 
 read -p "Enter version: " VERSION
 
-bpkg info set dependencies.@anchorlib/core="^$VERSION" -f packages/storage packages/react packages/router packages/react-classic packages/solid packages/svelte packages/vue irpclib/irpc
+bpkg info set dependencies.@anchorlib/core="^$VERSION" -f packages/storage packages/react packages/router packages/solid packages/svelte packages/vue irpclib/irpc
 bpkg info set dependencies.@anchorlib/router="^$VERSION" -f packages/react
-bpkg info set dependencies.@anchorlib/storage="^$VERSION" -f packages/react packages/react-classic packages/solid packages/svelte packages/vue
+bpkg info set dependencies.@anchorlib/storage="^$VERSION" -f packages/react packages/solid packages/svelte packages/vue
 bpkg info set peerDependencies.@irpclib/irpc="^$VERSION" -f irpclib/http irpclib/ws irpclib/broadcast
