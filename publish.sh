7ffdf6920983cb349986168679be0a92b60104a5
Nanang Mahdaen El Agung <mahdaen@gmail.com>
2026-05-19 16:35:16 +0700

    chore(publish): remove react-classic from release script
    Removes the `react-classic` package from the automated publish script, as it is no longer being released.
    
    - Remove the publish command for `packages/react-classic` from `publish.sh`.

--- publish.sh
+++ publish.sh
@@ -3,7 +3,6 @@ bpkg publish -f packages/core
 bpkg publish -f packages/storage
 bpkg publish -f packages/router
 bpkg publish -f packages/react
-bpkg publish -f packages/react-classic
 bpkg publish -f packages/solid
 bpkg publish -f packages/svelte
 bpkg publish -f packages/vue
